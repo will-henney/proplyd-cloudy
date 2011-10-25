ed155ba35798f4c31cd28111bdd42582c3519720
William Henney <will@henney.org>
2011-10-25 13:16:37 -0500

    Improve the root-finding method in auto-models.py
    Just use secant method to bracket the roots, then start again with brent method. This should be guaranteed to work in all circumstances, and initial tests suggest it will converge in fewer iterations. We also cache the function evaluations, so that we never have to run the same Cloudy model twice.

--- models/auto-models.py
+++ models/auto-models.py
@@ -75,7 +75,7 @@ def find_max_sound(ccmodel, use_T_not_c=False):
     m = claudia.CloudyModel(ccmodel.prefix, indir=ccmodel.indir, outdir=ccmodel.outdir)
     imax_sound = m.pre.cadwind_kms.argmax()
     imax_T = m.ovr.Te.argmax()
-    R = args.Rmax - m.ovr.depth/args.r0
+    R = cmdargs.Rmax - m.ovr.depth/cmdargs.r0
     print "Max sound at R=%.4f. Max Te at R=%.4f" % (R[imax_sound], R[imax_T])
     if use_T_not_c:
         return R[imax_T]
@@ -91,7 +91,7 @@ def find_ifront(ccmodel):
     # make a claudia model that corresponds to the cloudycontrol model
     m = claudia.CloudyModel(ccmodel.prefix, indir=ccmodel.indir, outdir=ccmodel.outdir)
     ifrac = 10**m.ovr.HII
-    R = args.Rmax - m.ovr.depth/args.r0
+    R = cmdargs.Rmax - m.ovr.depth/cmdargs.r0
     Rif = find_x_at_y(0.5, R, ifrac)
     return Rif
 
@@ -115,29 +115,29 @@ def find_x_at_y(ystar, x, y):
     xstar = x1 + (ystar-y1)*(x2-x1)/(y2-y1)
     return xstar
 
-def setup_and_run_cloudy_model(args, extras):
-    cloudy_cmd = ["time", args.cloudyexec] 
+def setup_and_run_cloudy_model(cmdargs, extras, monitor):
+    cloudy_cmd = ["time", cmdargs.cloudyexec] 
     # files are simply named after iteration number and density
-    filename = "it%.2in%.3ex0%.2f" % (extras.it, extras.hden, extras.x0)
+    filename = "it%.2in%.3ex0%.2f" % (monitor.it, extras.hden, extras.x0)
     print "Calculating " + os.path.join(extras.datapath, filename)
     m = Model(filename, dryrun=False, indir=extras.datapath, outdir=extras.datapath, 
               verbose=True, cloudy_cmd=cloudy_cmd)
-    if args.fastcloudy:
+    if cmdargs.fastcloudy:
         dust = "FastOrion"
     else:
         dust = "Orion"
-    m.write(physical.proplyd(args.r0, extras.hden, Rmax=args.Rmax, 
-                             W=args.W, x0=extras.x0, dust=dust))
+    m.write(physical.proplyd(cmdargs.r0, extras.hden, Rmax=cmdargs.Rmax, 
+                             W=cmdargs.W, x0=extras.x0, dust=dust))
     m.write(misc.optimize())
     m.write(misc.stopping())
-    if args.fastcloudy:
+    if cmdargs.fastcloudy:
         m.write(misc.iterate(iterations=0))
     else:
         m.write(misc.iterate())
     m.write(incident.background())
-    m.write(incident.star(extras.logPhiH, args.Tstar, atmosphere=args.atmosphere, log_g=4.1)) # th1C
+    m.write(incident.star(extras.logPhiH, cmdargs.Tstar, atmosphere=cmdargs.atmosphere, log_g=4.1)) # th1C
     m.write(save.default())
-    if args.savecont:
+    if cmdargs.savecont:
         m.write("""\
 save every last ionizing continuum 1.0 0 ".icont"
 """)
@@ -145,47 +145,101 @@ save every last ionizing continuum 1.0 0 ".icont"
     return m
     
 
+class BracketedException(Exception):
+    pass
+
+class Monitor(object):
+    """
+    Class for storing variables that monitor the state of the root-finding process
+
+    For instance, iteration number, list of previous x and y values, etc
+    
+    None of the variables stored here should materially affect the model calculations
+    """
+    def __init__(self, startiter=0, onlybracket=False):
+        self.it = startiter
+        # Dictionary for storing function evaluations with different hden or x0
+        self.cache = dict()
+        # x-values that most closely bracket the root
+        self.xpos = None
+        self.xneg = None
+        # In the first stage we only want to bracket the root
+        self.onlybracket = onlybracket
+        
+
+    def update(self, x, y):
+        # Update the cache
+        self.cache[x] = y
+        # Update the brackets
+        if y > 0.0:
+            if self.xpos is None or y < self.cache[self.xpos]:
+                self.xpos = x
+        else:
+            if self.xneg is None or y > self.cache[self.xneg]:
+                self.xneg = x
+
+        print "Current brackets: ", self.xneg, self.xpos
+        # Return True if we have brackets
+        return not (self.xpos is None or self.xneg is None)
+    
 
 
 
 
-
-def find_Rsonic_for_hden(hden_Rmax, args, extras):
+def find_Rsonic_for_hden(hden_Rmax, cmdargs, extras, monitor):
     """
     Runs a Cloudy model to find the dimensionless radius R of the sonic point
     
     Principal argument: hden_Rmax
 
-    Other vars are taken from args (command line parameters) and extras
+    Other vars are taken from cmdargs (command line parameters) and extras
 
     it - problematic when called as part of root finding routine
     """
     extras.hden = hden_Rmax
-    extras.it += 1
-    print "Running cloudy with hden = %.3e" % (hden_Rmax)
-    m = setup_and_run_cloudy_model(args, extras)
-    # Assume that sonic point is at sound speed maximum (actually we use Te)
-    return find_max_sound(m) - 1.0
+    if hden_Rmax in monitor.cache:
+        result = monitor.cache[hden_Rmax]
+        print "Using cached result for hden = %.3e" % (hden_Rmax)
+    else:
+        print "Running cloudy with hden = %.3e" % (hden_Rmax)
+        monitor.it += 1
+        m = setup_and_run_cloudy_model(cmdargs, extras, monitor)
+        result = find_max_sound(m) - 1.0
+        bracketed = monitor.update(hden_Rmax, result)
+        if monitor.onlybracket and bracketed:
+            # Option to give up on looking for this root by the first method
+            raise BracketedException
+    return result
 
-def find_Rif_for_x0(x0, args, extras):
+def find_Rif_for_x0(x0, cmdargs, extras, monitor):
     """
     Runs a Cloudy model to find the dimensionless radius R of the i-front
     
     Principal argument: x0
 
-    Other vars are taken from args (command line parameters) and extras
+    Other vars are taken from cmdargs (command line parameters) and extras
 
     it - problematic when called as part of root finding routine
     """
     extras.x0 = x0
-    dR = (0.5*args.W*extras.x0) / \
-        (args.r0*SIGMA0*extras.hden*args.Rmax*args.Rmax*velocity_law_U_of_R(args.Rmax))
-    extras.it += 1
-    print "Running cloudy with x0 = %.2f, dR = %.5f" % (x0, dR)
-    m = setup_and_run_cloudy_model(args, extras)
-    Rif = find_ifront(m)
-    print "I-front radius = %.4f, target = %.4f" % (Rif, (1.0 - dR))
-    return Rif - (1.0 - dR)
+    dR = (0.5*cmdargs.W*extras.x0) / \
+        (cmdargs.r0*SIGMA0*extras.hden*cmdargs.Rmax*cmdargs.Rmax*velocity_law_U_of_R(cmdargs.Rmax))
+    if x0 in monitor.cache:
+        result = monitor.cache[x0]
+        print "Using cached result for x0 = %.2f, dR = %.5f" % (x0, dR)
+    else:
+        print "Running cloudy with x0 = %.2f, dR = %.5f" % (x0, dR)
+        monitor.it += 1
+        m = setup_and_run_cloudy_model(cmdargs, extras, monitor)
+        Rif = find_ifront(m)
+        result = Rif - (1.0 - dR)
+        bracketed = monitor.update(x0, result)
+        if monitor.onlybracket and bracketed:
+            # Option to give up on looking for this root by the first method
+            raise BracketedException
+        print "I-front radius = %.4f, target = %.4f" % (Rif, (1.0 - dR))
+
+    return result
 
 
 
@@ -227,6 +281,8 @@ if __name__ == '__main__':
                         help="Thickness of i-front in units of mfp at 1 Rydberg")
     parser.add_argument("--initialx0", type=float, default=10.8,
                         help="Guess at offset between sonic point and i-front in units of W/2 (this parameter is adjusted until the models converge)")
+    parser.add_argument("--initialhden", type=float, default=0.0,
+                        help="Initial guess at outer density (if 0.0 then this will be estimated from Stroemgren condition)")
     parser.add_argument("--diffuseBeta", type=float, default=0.004,
                         help="Diffuse EUV flux to stellar flux ratio")
     parser.add_argument("--logPhiH", type=float, default=14.54,
@@ -241,45 +297,81 @@ if __name__ == '__main__':
                         help="Whether to make cloudy run faster (at the expense of accuracy)")
     parser.add_argument("--savecont", "-s", type=bool, default=False,
                         help="Whether to make cloudy save the continuum for every zone")
-    args = parser.parse_args()
+
+    
+    cmdargs = parser.parse_args()
 
     
     
-    if args.multiprocess:
+    if cmdargs.multiprocess:
         raise NotImplementedError # Delete this when it is implemented properly
         pool = multiprocessing.Pool() # by default makes one process per core
 
     # ID for this proplyd model
-    modelid = "%s%.6i-phi%.2f-r%.2f" % (args.atmosphere, args.Tstar, args.logPhiH, np.log10(args.r0))
+    modelid = "%s%.6i-phi%.2f-r%.2f" % (cmdargs.atmosphere, cmdargs.Tstar, 
+                                        cmdargs.logPhiH, np.log10(cmdargs.r0))
     # the ID may have to be refined by adding more parameters later
 
-    thetas = np.linspace(0.0, 90.0, args.ntheta)
+    thetas = np.linspace(0.0, 90.0, cmdargs.ntheta)
     for theta in thetas:
         extras = ExtraPars() # container for miscellaneous parameters
         thetaid = "th%.2i" % (theta)
         # All the files for a given angle now go in the same directory...
-        extras.datapath = os.path.join(args.rootdir, modelid, thetaid)
-        PhiH = (10**args.logPhiH) * (np.cos(theta * DEGREES) + args.diffuseBeta)
-        n0 = calculate_nominal_density(PhiH, args.r0)
+        extras.datapath = os.path.join(cmdargs.rootdir, modelid, thetaid)
+        PhiH = (10**cmdargs.logPhiH) * (np.cos(theta * DEGREES) + cmdargs.diffuseBeta)
+        if theta == 0.0:
+            PhiH0 = PhiH
+            if cmdargs.initialhden == 0.0:
+                # Only estimate the outer density if it was not specified on the command line
+                n0 = calculate_nominal_density(PhiH, cmdargs.r0)
+                hden_Rmax = calculate_hden_Rmax(cmdargs.Rmax, n0) # first guess for outer density
+            else:
+                hden_Rmax = cmdargs.initialhden
+            hden_Rmax0 = hden_Rmax
+        else:
+            hden_Rmax = hden_Rmax0 * np.sqrt(PhiH/PhiH0)
+            
         extras.logPhiH = np.log10(PhiH)
-        hden_Rmax = calculate_hden_Rmax(args.Rmax, n0) # first guess for outer density
-        extras.x0 = args.initialx0                     # Fix x0 while we converge Rsonic
-        converged = False
+        extras.x0 = cmdargs.initialx0                     # Fix x0 while we converge Rsonic
 
+        hdenmonitor = Monitor(startiter=0, onlybracket=True)
+        try: 
+            scipy.optimize.newton(find_Rsonic_for_hden, 
+                                  hden_Rmax, tol=cmdargs.tolerance**2, 
+                                  args=(cmdargs, extras, hdenmonitor))
+        except BracketedException as error:
+            print error
+            hdenmonitor.onlybracket = False
+            scipy.optimize.brentq(find_Rsonic_for_hden, 
+                                  a=hdenmonitor.xneg, b=hdenmonitor.xpos, 
+                                  xtol=cmdargs.tolerance*hdenmonitor.xneg,
+                                  args=(cmdargs, extras, hdenmonitor))
+        except RuntimeError as error: 
+            print "Ignoring RuntimeError: ", error
 
-        extras.it = 0
-        scipy.optimize.newton(find_Rsonic_for_hden, 
-                              hden_Rmax, tol=args.tolerance**2, args=(args, extras))
         print "Angle %i converged Rsonic after %i iteration%s" \
-            % (theta, extras.it, "s" if extras.it > 1 else "")
+            % (theta, hdenmonitor.it, "s" if hdenmonitor.it > 1 else "")
 
-        scipy.optimize.newton(find_Rif_for_x0, 
-                              args.initialx0, tol=args.tolerance**2, args=(args, extras))
+        x0monitor = Monitor(startiter=hdenmonitor.it, onlybracket=True)
+        try:
+            scipy.optimize.newton(find_Rif_for_x0, 
+                                  cmdargs.initialx0, tol=cmdargs.tolerance**2, 
+                                  args=(cmdargs, extras, x0monitor))
+        except BracketedException as error:
+            print error
+            x0monitor.onlybracket = False
+            scipy.optimize.brentq(find_Rif_for_x0,
+                                  a=x0monitor.xneg, b=x0monitor.xpos, 
+                                  xtol=cmdargs.tolerance*x0monitor.xneg,
+                                  args=(cmdargs, extras, x0monitor))
+        except RuntimeError as error: 
+            print "Ignoring RuntimeError: ", error
+
         print "Angle %i converged Rif after %i iteration%s" \
-            % (theta, extras.it, "s" if extras.it > 1 else "")
+            % (theta, x0monitor.it, "s" if x0monitor.it > 1 else "")
 
 
-    if args.multiprocess:
+    if cmdargs.multiprocess:
         # Clean up everything
         pool.close()
         pool.join()
