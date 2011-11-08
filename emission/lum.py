be628a659fabdba4bdd6cbe6f3005bffcc8103b3
Nahiely Flores <nahieflores@gmail.com>
2011-11-08 12:21:26 -0600

    algunas lineas mas a la luminosidad, problemas con el concepto

--- emission/lum.py
+++ emission/lum.py
@@ -1,11 +1,45 @@
 import numpy as np
 
-K_list=[5,6,7,8,9,10,11,12,13,14]
+Phi = [0,10,20,30,40,50,60,70,80,90] #Lista de angulos en Phi
+Theta = [0,10,20,30,40,50,60,70,80,90] #Lista de angulos en Theta
+Radi = [0,1,2,3,4,5,6,7,8,9] #Lista de pasos en z de cada modelo de Cloudy
+Emissivity = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,10.] #Lista de las emisividades de las lineas de Cloudy
 
-NK = len(K_list)
+NK = len(Phi)
+NJ = len(Theta)
+NI = len(Radi)
 
-for k in range(len(K_list)):
-    print k, K_list[k], k+1, K_list[k+1];
-    print K_list[k+1], K_list[k-1];
-    DPhi = (1./2.)*(K_list[k+1]-K_list[k-1]);
-    print DPhi
+sumR=0
+sumTheta=0
+sumPhi=0
+
+#La integral sobre r
+
+for j in range(NJ):
+    for i in range(NI):
+        ineg = max(0, i - 1)
+        ipos = min(NI-1, i + 1)
+        DR = 0.5*(Radi[ipos] - Radi[ineg])
+        DF = (Radi[i]**2) * Emissivity[i]
+        sumR = sumR + (DF*DR)
+    print "sumR =", sumR
+    
+#La integral sobre Theta
+
+for j in range(NJ):
+    jneg = max(0, j - 1)
+    jpos = min(NJ-1, j + 1)
+    DTheta = 0.5*(np.cos(np.radians(Theta[jpos])) - np.cos(np.radians(Theta[jneg])))
+    sumTheta = sumTheta + DTheta
+    print "sumTheta =", sumTheta
+
+#La integral sobre Phi
+
+for k in range(NK):
+    kneg = max(0, k - 1)
+    kpos = min(NK-1, k + 1)
+    DPhi = 0.5*(Phi[kpos] - Phi[kneg])
+    sumPhi = sumPhi + DPhi
+    print "sumPhi =", sumPhi
+
+
