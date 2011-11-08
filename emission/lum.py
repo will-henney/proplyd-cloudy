1be6977bf727304d2b5dc97ae8d33406476f7f79
William Henney <will@henney.org>
2011-11-08 13:12:18 -0600

    Correct nested loops and check that integral gives correct vlue for constant emissivity

--- emission/lum.py
+++ emission/lum.py
@@ -9,37 +9,39 @@ NK = len(Phi)
 NJ = len(Theta)
 NI = len(Radi)
 
-sumR=0
-sumTheta=0
-sumPhi=0
-
-#La integral sobre r - edit por Will
-
-for j in range(NJ):
-    for i in range(NI):
-        ineg = max(0, i - 1)
-        ipos = min(NI-1, i + 1)
-        DR = 0.5*(Radi[ipos] - Radi[ineg])
-        DF = (Radi[i]**2) * Emissivity[i]
-        sumR = sumR + (DF*DR)
-    print "sumR =", sumR
-    
-#La integral sobre Theta
-
-for j in range(NJ):
-    jneg = max(0, j - 1)
-    jpos = min(NJ-1, j + 1)
-    DTheta = 0.5*(np.cos(np.radians(Theta[jpos])) - np.cos(np.radians(Theta[jneg])))
-    sumTheta = sumTheta + DTheta
-    print "sumTheta =", sumTheta
-
-#La integral sobre Phi
-
+sumEmiss = 0.0
+#La integral sobre r, th, ph
 for k in range(NK):
     kneg = max(0, k - 1)
     kpos = min(NK-1, k + 1)
-    DPhi = 0.5*(Phi[kpos] - Phi[kneg])
-    sumPhi = sumPhi + DPhi
-    print "sumPhi =", sumPhi
+    DPhi = 0.5*np.radians(Phi[kpos] - Phi[kneg])
+    for j in range(NJ):
+        jneg = max(0, j - 1)
+        jpos = min(NJ-1, j + 1)
+        DMu = -0.5*(np.cos(np.radians(Theta[jpos])) - np.cos(np.radians(Theta[jneg])))
+        for i in range(NI):
+            ineg = max(0, i - 1)
+            ipos = min(NI-1, i + 1)
+            DR = 0.5*(Radi[ipos] - Radi[ineg])
+            DVol =  DPhi * DMu * (Radi[i]**2) * DR
+            sumEmiss += DVol
+
+print "sumEmiss =", sumEmiss
+print "Volume = ",  (4.*np.pi/3.) * (Radi[-1]**3 - Radi[0]**3) * 0.5 * 0.25
+
+# #La integral sobre Theta
+
+# for j in range(NJ):
+#     sumTheta = sumTheta + DTheta
+#     print "sumTheta =", sumTheta
+
+# #La integral sobre Phi
+
+# for k in range(NK):
+#     kneg = max(0, k - 1)
+#     kpos = min(NK-1, k + 1)
+#     DPhi = 0.5*(Phi[kpos] - Phi[kneg])
+#     sumPhi = sumPhi + DPhi
+#     print "sumPhi =", sumPhi
 
 
