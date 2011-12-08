fbffc5103c1a7ecf654a8f097c653152c78c4138
William Henney <will@henney.org>
2011-12-08 13:21:46 -0600

    Fix sphere_profile.py - now it seems to work for individual lines

--- emission/sphere_profile.py
+++ emission/sphere_profile.py
@@ -6,9 +6,9 @@ import argparse
 sys.path.append("../src")       # make sure we can find claudia.py
 import claudia
 
-if __name__ == '__main__':
-    import warnings, argparse
-    warnings.filterwarnings("ignore")
+# Avoid verbose error messages from numpy during the reading of the Cloudy files
+import warnings, argparse
+warnings.filterwarnings("ignore")
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(
@@ -30,29 +30,40 @@ parser.add_argument(
     "--inc", type=float, default=0.0,
     help='Inclination in degrees')
 
+parser.add_argument(
+    "--r0", type=float, default=8.0e14,
+    help='Radius of proplyd ionization front in cm')
+parser.add_argument(
+    "--Rmax", type=float, default=9.0,
+    help='Radius of outer face of cloudy model in units of r0')
+
+parser.add_argument(
+    "--emline", type=str, default="H__1__6563A",
+    help="Which emission line to use from the Cloudy model")
+
 cmd_args = parser.parse_args()
 
 # Set some local variables from the command line arguments
 NU = cmd_args.nvel
 inc_degrees = cmd_args.inc
-modeldir, modelname = os.path.split(cmd_args.modelpath)
+cmd_args.modeldir, cmd_args.modelname = os.path.split(cmd_args.modelpath)
 
 # Read in Cloudy model
-if modeldir != '':
-    claudia.CloudyModel.indir = modeldir
-    claudia.CloudyModel.outdir = modeldir
+if cmd_args.modeldir != '':
+    claudia.CloudyModel.indir = cmd_args.modeldir
+    claudia.CloudyModel.outdir = cmd_args.modeldir
 else:
     claudia.CloudyModel.indir = '.'
     claudia.CloudyModel.outdir = '.'
 
 #set de variables of the model that are defined in *.in
 
-r0 = 8.0e14
-Rmax = 9.0
+r0 = cmd_args.r0
+Rmax = cmd_args.Rmax
 
 #The model
 
-m = claudia.CloudyModel(modelname) 
+m = claudia.CloudyModel(cmd_args.modelname) 
     
 cosi = np.cos(np.radians(inc_degrees))
 sini = np.sin(np.radians(inc_degrees))
@@ -68,15 +79,17 @@ NK = len(Phi)
 NJ = len(Theta)
 NI = len(Radius)
 
-R = Rmax - Radius/r0
-i2 = len(R[R>=1.0])
+R = Rmax - Radius/r0            # array of dimensionless radius
+i2 = len(R[R>=1.0])             # find index where R = 1
 
 Density = 10**(m.ovr.hden)
-Emissivity = 10**(m.em.H__1__6563A)
+Emissivity = 10**(m.em[cmd_args.emline])
+sound_speed = np.sqrt(3./5.)*m.pre.cadwind_kms # isothermal sound speed
 
 n0 = Density[i2]
 
-Velocity = n0 / (R**2 * Density)
+
+Velocity = sound_speed[i2] * n0 / (R**2 * Density) 
 
 # Define the velocity bins
 umax = max(abs(Velocity))
@@ -88,35 +101,32 @@ sumEmiss = 0.0
 
 #Line profile
 
-for line in m.em.dtype.names:
-    print line
-    for k in range(NK):
-        cphi = np.cos(Phi[k])
-        sphi = np.sin(Phi[k])
-        kneg = max(0, k - 1)
-        kpos = min(NK-1, k + 1)
-        dphi = 0.5*(Phi[kpos] - Phi[kneg])
-        for j in range(NJ):
-            ctheta = np.cos(Theta[j])
-            stheta = np.sin(Theta[j])
-            jneg = max(0, j - 1)
-            jpos = min(NJ-1, j + 1)
-            dmu = -0.5*(np.cos(Theta[jpos]) - np.cos(Theta[jneg]))
-            for i in range(NI):
-                ineg = max(0, i - 1)
-                ipos = min(NI-1, i + 1)
-                dr = 0.5*(Radius[ipos] - Radius[ineg])
-                dvol =  dphi * dmu * (Radius[i]**2) * dr
-                sumEmiss += dvol * Emissivity[i]
-                u = -Velocity[i]*(sini*stheta*cphi + cosi*ctheta)
-                x = (u-umin)/du
-                iu = int(x)
-                assert iu >= 0 and iu < NU, "Index (%i) out of bounds [%i:%i] of velocity array" % (iu, 0, NU-1)
-                Perfil[iu] += dvol * Emissivity[i]
-    PerfilU = np.linspace(umin, umax, NU)
-    savefile = "%(modelname)s-perfil-NU%(NU)i-inc%(inc_degrees)i-%(line)s.dat" % (locals())
-# savefile = "%(modelname)s-perfil-N%(N)i-NU%(NU)i-inc%(inc_degrees)i.dat" % (locals())
-    np.savetxt(savefile, (PerfilU, Perfil))
+for k in range(NK):
+    cphi = np.cos(Phi[k])
+    sphi = np.sin(Phi[k])
+    kneg = max(0, k - 1)
+    kpos = min(NK-1, k + 1)
+    dphi = 0.5*(Phi[kpos] - Phi[kneg])
+    for j in range(NJ):
+        ctheta = np.cos(Theta[j])
+        stheta = np.sin(Theta[j])
+        jneg = max(0, j - 1)
+        jpos = min(NJ-1, j + 1)
+        dmu = -0.5*(np.cos(Theta[jpos]) - np.cos(Theta[jneg]))
+        for i in range(NI):
+            ineg = max(0, i - 1)
+            ipos = min(NI-1, i + 1)
+            dr = 0.5*(Radius[ipos] - Radius[ineg])
+            dvol =  dphi * dmu * (Radius[i]**2) * dr
+            sumEmiss += dvol * Emissivity[i]
+            u = -Velocity[i]*(sini*stheta*cphi + cosi*ctheta)
+            x = (u-umin)/du
+            iu = int(x)
+            assert iu >= 0 and iu < NU, "Index (%i) out of bounds [%i:%i] of velocity array" % (iu, 0, NU-1)
+            Perfil[iu] += dvol * Emissivity[i]
+PerfilU = np.linspace(umin, umax, NU)
+savefile = "%(modelname)s-perfil-ntheta%(ntheta)i-nphi%(nphi)i-nvel%(nvel)i-inc%(inc)i-%(emline)s.dat" % (vars(cmd_args))
+np.savetxt(savefile, (PerfilU, Perfil))
 
 Volume = (4.*np.pi/3.) * (Radius[-1]**3 - Radius[0]**3) * 0.5
 print "sumEmiss =", sumEmiss
