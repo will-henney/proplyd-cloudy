40afa56222f45a513ff5bc556ff54fe3bfa62e41
William Henney <will@henney.org>
2011-12-09 12:46:11 -0600

    Untested version of model_profile.py - uses only the theta angles that exist

--- emission/model_profile.py
+++ emission/model_profile.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy
-import sys, os
+import sys, os, glob
 import argparse
 
 sys.path.append("../src")       # make sure we can find claudia.py
@@ -12,14 +12,14 @@ warnings.filterwarnings("ignore")
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(
-    description="Calculate line profile of spherical Cloudy model")
+    description="Calculate line profile of Cloudy proplyd model")
 
 parser.add_argument(
-    "modelpath", type=str,
-    help='Path to Cloudy model (assumes input and output in same directory)')
-parser.add_argument(
-    "--ntheta", type=int, default=100,
-    help='Number of angles theta')
+    "--modeldir", type=str, default=".",  
+    help='Path to proplyd directory model')
+# parser.add_argument(
+#     "--ntheta", type=int, default=100,
+#     help='Number of angles theta')
 parser.add_argument(
     "--nphi", type=int, default=200,
     help='Number of angles phi')
@@ -46,24 +46,45 @@ cmd_args = parser.parse_args()
 # Set some local variables from the command line arguments
 NU = cmd_args.nvel
 inc_degrees = cmd_args.inc
-cmd_args.modeldir, cmd_args.modelname = os.path.split(cmd_args.modelpath)
-
-# Read in Cloudy model
-if cmd_args.modeldir != '':
-    claudia.CloudyModel.indir = cmd_args.modeldir
-    claudia.CloudyModel.outdir = cmd_args.modeldir
-else:
-    claudia.CloudyModel.indir = '.'
-    claudia.CloudyModel.outdir = '.'
 
 #set de variables of the model that are defined in *.in
 
 r0 = cmd_args.r0
 Rmax = cmd_args.Rmax
 
+thetadirs =  glob.glob(os.path.join(cmd_args.modeldir, "th??"))
+
+# The following arrays are defined for each angle:
+# Radius, Velocity, Emissivity
+Radius_vectors = []
+Velocity_vectors = []
+Emissivity_vectors = []
+for thetadir in thetadirs:
+    # Read in the Cloudy model for each angle
+    claudia.CloudyModel.indir = os.path.join(cmd_args.modeldir, thetadir)
+    claudia.CloudyModel.outdir = claudia.CloudyModel.indir
+    # find the last iteration
+    modelname = glob.glob(os.path.join(claudia.CloudyModel.indir, "*.in"))[-1]
+    m = claudia.CloudyModel(modelname) 
+    Radius = m.ovr.depth #Lista de pasos en z de cada modelo de Cloudy
+    R = Rmax - Radius/r0            # array of dimensionless radius
+    i2 = len(R[R>=1.0])             # find index where R = 1
+
+    # Calculate physical variables as function of radius for this theta
+    Density = 10**(m.ovr.hden)
+    Emissivity = 10**(m.em[cmd_args.emline])
+    sound_speed = np.sqrt(3./5.)*m.pre.cadwind_kms # isothermal sound speed
+    n0 = Density[i2]
+    Velocity = sound_speed[i2] * n0 / (R**2 * Density) 
+
+    # Add them to the lists of vectors 
+    Radius_vectors.append(Radius)
+    Velocity_vectors.append(Velocity)
+    Emissivity_vectors.append(Emissivity)
+    
+
 #The model
 
-m = claudia.CloudyModel(cmd_args.modelname) 
     
 cosi = np.cos(np.radians(inc_degrees))
 sini = np.sin(np.radians(inc_degrees))
@@ -73,26 +94,15 @@ phimin, phimax = 0.0, np.radians(360.0)
 thetamin, thetamax = 0.0, np.radians(90.0)
 
 Phi = np.linspace(phimin, phimax, num=cmd_args.nphi) #Lista de angulos en Phi
-Theta = np.linspace(thetamin, thetamax, num=cmd_args.ntheta) #Lista de angulos en Theta
-Radius = m.ovr.depth #Lista de pasos en z de cada modelo de Cloudy
+# For the moment, we use the existing thetas with no interpolation
+Theta = np.array([float(s[2:]) for s in thetadirs])
+# Theta = np.linspace(thetamin, thetamax, num=cmd_args.ntheta) #Lista de angulos en Theta
 NK = len(Phi)
 NJ = len(Theta)
-NI = len(Radius)
 
-R = Rmax - Radius/r0            # array of dimensionless radius
-i2 = len(R[R>=1.0])             # find index where R = 1
-
-Density = 10**(m.ovr.hden)
-Emissivity = 10**(m.em[cmd_args.emline])
-sound_speed = np.sqrt(3./5.)*m.pre.cadwind_kms # isothermal sound speed
-
-n0 = Density[i2]
-
-
-Velocity = sound_speed[i2] * n0 / (R**2 * Density) 
 
 # Define the velocity bins
-umax = max(abs(Velocity))
+umax = max(abs(Velocity_vectors[0]))
 umin = -umax
 du = (umax - umin)/NU
 Perfil = np.zeros(NU)
@@ -108,11 +118,17 @@ for k in range(NK):
     kpos = min(NK-1, k + 1)
     dphi = 0.5*(Phi[kpos] - Phi[kneg])
     for j in range(NJ):
+        # for each angle
+        Radius = Radius_vectors[j]
+        Velocity = Velocity_vectors[j]
+        Emissivity = Emissivity_vectors[j]
+
         ctheta = np.cos(Theta[j])
         stheta = np.sin(Theta[j])
         jneg = max(0, j - 1)
         jpos = min(NJ-1, j + 1)
         dmu = -0.5*(np.cos(Theta[jpos]) - np.cos(Theta[jneg]))
+        NI = len(Radius)
         for i in range(NI):
             ineg = max(0, i - 1)
             ipos = min(NI-1, i + 1)
