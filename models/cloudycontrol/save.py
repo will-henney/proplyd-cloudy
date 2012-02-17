01dfa78e8b9164224bec08766962fb175c1652fd
William Henney <will@henney.org>
2012-02-16 22:03:23 -0600

    Correct some of the line labels in cloudycontrol/save.py and add some more
    + We should now have all the lines from the Tsamis paper

--- models/cloudycontrol/save.py
+++ models/cloudycontrol/save.py
@@ -38,28 +38,28 @@ O  3  4959A  // [O III] nebular line
 O  3  5007A  // ditto
 S II  6731A  // [S II] nebular line 
 S II  6716A  // ditto
-* COMMENTED S  2  4074A  // [S II] auroral lines (sum of 4068+4076) 
 S II  4070A  // [S II] auroral line (4068A in Mesa-Delgado)
 S II  4078A  // [S II] auroral line (4076A in Mesa-Delgado)
-O  1  6300A  // [O I] 6300.3
 S  3  6312A  // [S III] 6312.10
-Ar 3  7135A  // [Ar III] 7135.78
+O  1  6300A  // [O I] 6300.3
+O  1  5577A  // [O I]
 O II  3729A  // [O II] 
 O II  3726A  // [O II]
-Ne 3  3869A  // [Ne III] 
 O II  7323A  // [O II] (7320 in Tsamis)
 O II  7332A  // [O II] (7330 in Tsamis)
+Ne 3  3869A  // [Ne III] 
 Ar 3  5192A  // [Ar III]
+Ar 3  7135A  // [Ar III] 7135.78
 Ar 4  4740A  // [Ar IV]
-O  1  5577A  // [O I]
-Cl 3  5518A  // [Cl III]
 Ar 4  4740A  // [Ar IV]
 Ar 4  4711A  // [Ar IV]
-* Metal recombination lines
-O  2  4651A   // O II 
-C  2  4267A   // C II
+Cl 3  5518A  // [Cl III]
+Cl 3  5538A  // [Cl III] given as 5539A in Tsamis et al 2011
+* Metal optical recombination lines
+O 2r  4651A   // O II 4651 total recombination, 4638.86-4696.35 (8 lines) 
+C  2  4267A   // C II recombination line
 * Fluorescent lines
-TOTr  5199A  // [N I] line
+TOTL  5199A  // [N I] line
 * Iron lines for comparison with Mesa-Delgado
 Fe 3  5271A
 Fe 3  4988A
@@ -74,7 +74,7 @@ TOTL  2326A  // C II] Semi-forbidden
 C  3 1910A   // C II] Semi-forbidden
 C  3 1907A   // [C II] forbidden
 O II  2471A  // [O II] forbidden (2470 in Tsamis)
-* FIR lines
+* FIR lines, just in case
 Ne 2 12.81m
 Ne 3 15.55m
 end of lines
